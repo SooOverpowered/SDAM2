c887b8ff57cfbc90b322f1be9f774bc2f7b348c1
SooOverpowered <lehoangminh15122000@gmail.com>
2021-05-26 16:00:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDAM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
