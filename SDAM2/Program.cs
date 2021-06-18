8b5aef2e0774ad33fc0c5a43758316335df041f4
SooOverpowered <lehoangminh15122000@gmail.com>
2021-06-19 00:29:15 +0700

    remove console window sizing for compatibility with other OS

--- SDAM2/Program.cs
+++ SDAM2/Program.cs
@@ -10,7 +10,6 @@ namespace SDAM2
     {
         static void Main()
         {
-            Console.SetWindowSize(100, 25);
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             WindowManager window = new WindowManager();
